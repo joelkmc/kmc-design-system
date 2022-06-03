61a4a5ba66bb610d656f014cbc10a0e7e95eb922
joelcabagua <cabagua.joel@gmail.com>
2022-06-03 16:33:58 +0800

    Refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
-import './App.css'
 
 function App() {
   return (
     <div className='App'>
-      <p>learn react</p>
+      <p className='p-10 bg-red-300'>learn react</p>
     </div>
   )
 }
