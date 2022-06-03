e6c5f5ae6c210c5cc720322a1cae892350e0a006
joelcabagua <cabagua.joel@gmail.com>
2022-06-03 15:15:20 +0800

    Refactor

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,10 @@ import './App.css'
 
 function App() {
   const dadta = 'asdasd'
+  const dadta = 'asdasd'
+  const dadta = 'asdasd'
+  const dadta = 'asdasd'
+  const dadta = 'asdasd'
   return (
     <div className='App'>
       <p>learn react</p>
